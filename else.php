fd5bb2a0a62fd48baf0347a39e30880f7c12f4a8
Sy Hung <nguyensyhung689@gmail.com>
2019-07-12 10:01:51 +0700

    Statements else and elseif

---
+++ else.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Else and elseif Statements</title>
+</head>
+
+<body>
+<?php
+    $a = 5 ; $b = 20 ; 
+    if ($a > $b) {
+        echo "A is larger than B";
+    }
+    elseif($a < $b){
+        echo " A is smaller than B";
+    }
+    else " S is equal than B";
+?>
+</body>
+
+</html>
