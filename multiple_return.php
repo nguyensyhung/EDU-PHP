45d414335dd73a01d8d844a5b901c4843f1778e7
Sy Hung <nguyensyhung689@gmail.com>
2019-07-15 21:39:59 +0700

    Function: Multiple Returns

---
+++ multiple_return.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Functions: Multiple Returns</title>
+</head>
+
+<body>
+<?php 
+    function add_subt($num1, $num2)
+    {
+        $add = $num1 + $num2;
+        $subt = $num1 - $num2;
+        return array($add, $subt);
+    }
+    $result_array = add_subt(10, 5);
+    echo "Add: ". $result_array[0] . "<br />";
+    echo "Subt: ". $result_array[1] . "<br />";
+
+    list($add_result, $subt_result) = add_subt(20, 7);
+    echo "Add: ". $add_result . "<br />";
+    echo "Subt: ". $subt_result . "<br />"; 
+?>
+</body>
+
+</html>
